5356449736ef931e0853701b9a1c1342fa92befd
osrpt <yangsibai@gmail.com>
2014-05-09 23:08:43 +0800

    添加对dropbox数据存储的控制

--- js/background.js
+++ js/background.js
@@ -6,18 +6,21 @@ client.authDriver(new Dropbox.AuthDriver.ChromeExtension({
     receiverPath: 'dropbox/chrome_oauth_receiver.html'
 }));
 
-readFile(function (err, content) {
+var isCommunicateDropbox = false;
+
+
+readFile(function(err, content) {
     if (!err) {
         localStorage.data = content || '{}';
     }
 });
 
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
+    function(request, sender, sendResponse) {
         var cmd = request.cmd;
         if (cmd === 'getTags') {
             var data = JSON.parse(localStorage.data);
-            data.tags.sort(function (a, b) {
+            data.tags.sort(function(a, b) {
                 return b.links.length - a.links.length;
             });
             sendResponse(data.tags);
@@ -35,9 +38,9 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     chrome.pageAction.show(tabId);
-    if (getBookmark(tab.url)) {//如果包含了该地址
+    if (getBookmark(tab.url)) { //如果包含了该地址
         chrome.pageAction.setIcon({
             tabId: tabId,
             path: "icons/heart_b_24.png"
@@ -50,7 +53,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
  * get bookmark by url,if not exist,return null
  * @param url
  */
-var getBookmark = function (url) {
+var getBookmark = function(url) {
     if (typeof url === 'undefined') {
         return null;
     }
@@ -66,13 +69,12 @@ var getBookmark = function (url) {
     return null;
 };
 
-var fetchBookmarks = function (node, folderName, list) {
+var fetchBookmarks = function(node, folderName, list) {
     if (typeof node.children !== 'undefined') {
         for (var i = 0; i < node.children.length; i++) {
             fetchBookmarks(node.children[i], node.title, list);
         }
-    }
-    else {
+    } else {
         list.push({
             "id": node.id,
             "tags": folderName || 'unknown',
@@ -83,12 +85,10 @@ var fetchBookmarks = function (node, folderName, list) {
     }
 };
 
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onInstalled.addListener(function() {
     var data = {
-        "tags": [
-        ],
-        "bookmarks": [
-        ]
+        "tags": [],
+        "bookmarks": []
     };
 
     chrome.notifications.create('notify', {
@@ -97,11 +97,11 @@ chrome.runtime.onInstalled.addListener(function () {
         message: '正在转移书签数据',
         iconUrl: "/icons/heart_r_64.png",
         progress: 0
-    }, function (notificationId) {
+    }, function(notificationId) {
         /**
          * 初始化书签
          */
-        chrome.bookmarks.getTree(function (results) {
+        chrome.bookmarks.getTree(function(results) {
             try {
                 var list = [];
                 fetchBookmarks(results[0], '', list);
@@ -111,40 +111,37 @@ chrome.runtime.onInstalled.addListener(function () {
                     var progress = parseInt((i + 1) * 100 / list.length);
                     chrome.notifications.update(notificationId, {
                         progress: progress
-                    }, function () {
+                    }, function() {
                         if (progress === 100) {
-                            setTimeout(function () {
-                                chrome.notifications.clear(notificationId, function () {
-                                });
+                            setTimeout(function() {
+                                chrome.notifications.clear(notificationId, function() {});
                             }, 3 * 1000); //3秒钟之后清除
                         }
                     });
                 }
                 localStorage.data = JSON.stringify(data);
-                updateFile(function (err) {
+                updateFile(function(err) {
                     if (err) {
                         chrome.notifications.create('notify', {
                             type: 'basic',
                             title: 'Error',
                             message: 'Can not save data in dropbox,error:' + err.message,
                             iconUrl: '/icons/error_64.png'
-                        }, function () {
+                        }, function() {
 
                         });
-                    }
-                    else {
+                    } else {
                         chrome.notifications.create('notify', {
                             type: 'basic',
                             title: 'Success',
                             message: 'Save data in dropbox successfully',
                             iconUrl: '/icons/success_64.png'
-                        }, function () {
+                        }, function() {
 
                         });
                     }
                 });
-            }
-            catch (e) {
+            } catch (e) {
                 console.dir(e);
             }
         });
@@ -157,6 +154,7 @@ chrome.runtime.onInstalled.addListener(function () {
  * @param title
  * @param url
  */
+
 function addBookmark(data, obj, save, cb) {
     try {
         var title = obj.title;
@@ -185,12 +183,10 @@ function addBookmark(data, obj, save, cb) {
                 data.tags.push({
                     "name": tmpTag,
                     "lastModifyTime": new Date().getTime(),
-                    links: [
-                        {
-                            "title": title,
-                            "url": url
-                        }
-                    ]
+                    links: [{
+                        "title": title,
+                        "url": url
+                    }]
                 });
             }
         }
@@ -221,8 +217,7 @@ function addBookmark(data, obj, save, cb) {
             localStorage.data = JSON.stringify(data);
             updateFile(cb);
         }
-    }
-    catch (e) {
+    } catch (e) {
         console.dir(e);
     }
 }
@@ -230,52 +225,63 @@ function addBookmark(data, obj, save, cb) {
 /**
  * update data file in dropbox
  */
-function updateFile(cb) {
-    client.authenticate(function (err, client) {
-        if (err) {
-            if (typeof cb === 'function') {
-                cb(err);
-            }
-        }
-        else {
-            client.writeFile('data.json', localStorage.data || {}, function (err, stat) {
+
+var updateFile = function(cb) {
+    if (isCommunicateDropbox) {
+        setTimeout(function() {
+            updateFile(cb);
+        }, 5 * 1000);
+    } else {
+        isCommunicateDropbox = true;
+        client.authenticate(function(err, client) {
+            if (err) {
                 if (typeof cb === 'function') {
-                    cb(err, stat);
+                    cb(err);
                 }
-            });
-        }
-    });
+            } else {
+                client.writeFile('data.json', localStorage.data || {}, function(err, stat) {
+                    isCommunicateDropbox = false;
+                    if (typeof cb === 'function') {
+                        cb(err, stat);
+                    }
+                });
+            }
+        });
+    }
 }
 
 /**
  * 读取dropbox的文件数据
  * @param cb
  */
-function readFile(cb) {
-    client.authenticate(function (err, client) {
-        if (err) {
-            cb(err);
-        }
-        else {
-            client.readFile('data.json', function (err, content, stat, rangeInfo) {
-                cb(err, content);
-            });
-        }
-    });
-}
 
-/**
- * 去掉数据组重复的项
- * @param array
- * @return {Array}
- */
-function trimRepeat(array) {
-    var result = [];
-    for (var i = 0; i < array.length; i++) {
-        var tmp = array[i];
-        if (result.indexOf(tmp) === -1) {
-            result.push(tmp);
-        }
+    function readFile(cb) {
+        isCommunicateDropbox = true;
+        client.authenticate(function(err, client) {
+            if (err) {
+                cb(err);
+            } else {
+                client.readFile('data.json', function(err, content, stat, rangeInfo) {
+                    isCommunicateDropbox = false;
+                    cb(err, content);
+                });
+            }
+        });
     }
-    return result;
-}
+
+    /**
+     * 去掉数据组重复的项
+     * @param array
+     * @return {Array}
+     */
+
+    function trimRepeat(array) {
+        var result = [];
+        for (var i = 0; i < array.length; i++) {
+            var tmp = array[i];
+            if (result.indexOf(tmp) === -1) {
+                result.push(tmp);
+            }
+        }
+        return result;
+    }
